c71e174cca1c361e49380f567b5e89e3026dd130
SyedWafiHasan <syedwafihasan@gmail.com>
2017-10-20 23:21:19 +0530

    Version 1.0 released

--- app/src/main/java/com/wafihasan/ratecalculator/MainActivity.java
+++ app/src/main/java/com/wafihasan/ratecalculator/MainActivity.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     double baseRateValue = 0.0;
     double discountReceivedValue = 0.0;
     double gstValue = 0.0;
@@ -30,10 +30,10 @@ public class MainActivity extends AppCompatActivity
     EditText mrp;
 
     Button calculate;
+    Button clear;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -47,14 +47,27 @@ public class MainActivity extends AppCompatActivity
         gst = (EditText) findViewById(R.id.gst);
         mrp = (EditText) findViewById(R.id.mrp);
 
-        calculate = (Button) findViewById(R.id.button);
+        calculate = (Button) findViewById(R.id.calculate);
+        clear = (Button) findViewById(R.id.clear);
 
-        calculate.setOnClickListener(new View.OnClickListener()
-        {
+        calculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 calculate();
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                netRate.setText("");
+                profitPercent.setText("");
+                profitSP.setText("");
+                discountedProfit.setText("");
+                gst.setText("");
+                baseRate.setText("");
+                discountReceived.setText("");
+                mrp.setText("");
             }
         });
     }
@@ -62,24 +75,31 @@ public class MainActivity extends AppCompatActivity
 
     public void calculate()
     {
-        baseRateValue = Double.parseDouble(baseRate.getText().toString());
-        discountReceivedValue = Double.parseDouble(discountReceived.getText().toString());
-        gstValue = Double.parseDouble(gst.getText().toString());
-        mrpValue = Double.parseDouble(mrp.getText().toString());
+        if(gst.getText().toString().length() == 0 || baseRate.getText().toString().length() == 0 ||
+                discountReceived.getText().toString().length() == 0 || mrp.getText().toString().length() == 0)
+        {
+            Toast.makeText(getApplicationContext(), "Enter all values!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            baseRateValue = Double.parseDouble(baseRate.getText().toString());
+            discountReceivedValue = Double.parseDouble(discountReceived.getText().toString());
+            gstValue = Double.parseDouble(gst.getText().toString());
+            mrpValue = Double.parseDouble(mrp.getText().toString());
 
+            DecimalFormat decimalFormat = new DecimalFormat("0.00");
+            netRateValue = baseRateValue * 1.00 * (1 - (discountReceivedValue / 100)) * (1 + (gstValue / 100));
+            netRate.setText(decimalFormat.format(netRateValue));
 
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        netRateValue = baseRateValue * 1.02 * (1 - (discountReceivedValue/100)) * (1 + (gstValue/100));
-        netRate.setText(decimalFormat.format(netRateValue));
+            profitPercentValue = ((mrpValue - netRateValue) * 100 / netRateValue);
+            profitPercent.setText(decimalFormat.format(profitPercentValue));
 
-        profitPercentValue = ((mrpValue - netRateValue) * 100/netRateValue);
-        profitPercent.setText(decimalFormat.format(profitPercentValue));
+            discountProfitValue = (mrpValue * 0.95 - netRateValue) * (100 / netRateValue);
+            discountedProfit.setText(decimalFormat.format(discountProfitValue));
 
-        discountProfitValue = (mrpValue * 0.95 - netRateValue) * (100/netRateValue);
-        discountedProfit.setText(decimalFormat.format(discountProfitValue));
-
-        profitSPValue = (mrpValue*0.95 - netRateValue) * (100/(mrpValue*0.95));
-        profitSP.setText(decimalFormat.format(profitSPValue));
+            profitSPValue = (mrpValue * 0.95 - netRateValue) * (100 / (mrpValue * 0.95));
+            profitSP.setText(decimalFormat.format(profitSPValue));
+        }
     }
 }
 //net rate = base rate * 1.02 * (1- discount/100) + (1 + gst/100
